80633fe2884ac1f1081c479a4b017558d76e3de4
Alex <alex@vontis.nl>
2023-05-30 17:10:52 +0200

    remove account ids

--- variables.tf
+++ variables.tf
@@ -1,16 +1,13 @@
+variable "security_account_id" {
+  type    = string
+}
+variable "account_ids" {
+  type    = list(string)
+}
+
 variable "prefix" {
   type    = string
   default = "prowler-scanner"
-}
-
-variable "security_account_id" {
-  type    = string
-  default = "564033685323"
-}
-
-variable "account_ids" {
-  type    = list(string)
-  default = ["564033685323", "881042394954", "894243019954"]
 }
 variable "use_nat_gateway" {
   type    = bool
