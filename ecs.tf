88c3680a5503e2e8d49923f4715021390c978a66
Alex <alex@vontis.nl>
2023-07-28 12:23:54 +0200

    format code

--- ecs.tf
+++ ecs.tf
@@ -39,6 +39,7 @@ resource "aws_ecs_task_definition" "taskdef" {
 
 module "task_sg" {
   source                                = "terraform-aws-modules/security-group/aws"
+  version                               = "5.1.0"
   name                                  = "${var.prefix}-task"
   vpc_id                                = module.vpc.vpc_id
   ingress_with_source_security_group_id = []
--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,12 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.2"
-
-  name = "${var.prefix}-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b", "${data.aws_region.current.name}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
+  source                 = "terraform-aws-modules/vpc/aws"
+  version                = "4.0.2"
+  name                   = "${var.prefix}-vpc"
+  cidr                   = "10.0.0.0/16"
+  azs                    = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b", "${data.aws_region.current.name}c"]
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway     = var.use_nat_gateway
   one_nat_gateway_per_az = true
   enable_vpn_gateway     = false
-
 }
