6c30bd74d161c590d87df4c8abdeed5f940b737e
Alex <alex@vontis.nl>
2023-07-28 12:36:51 +0200

    cheap

---
+++ examples/cheap/main.tf
@@ -0,0 +1,9 @@
+// this module scans the account its run in (you still need to create the scanrole manually in 564033685323)
+module "prowler" {
+  source = "../../"
+  security_account_id = "496492991232"
+  account_ids         = ["496492991232"]
+  schedule_expression = "rate(5 minutes)"
+  use_nat_gateway = false
+  prefix = "prowler-cheap"
+}
